aea17cd3d85bcab52ddcb660b4dac5dba19cc074
Andrey.Speransky <andrey.speransky@gmail.com>
2020-10-20 23:33:52 +0300

    Создан шаблон post_list.html

--- djgirls/settings.py
+++ djgirls/settings.py
@@ -40,10 +40,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'djgirls.urls'
 
+TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
+# TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [TEMPLATE_DIR]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
