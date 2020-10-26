9c8c2f0f575581ee2001b98392f8e84aa0d59239
Andrey.Speransky <andrey.speransky@gmail.com>
2020-10-27 02:33:10 +0300

    ADD function to delete published posts

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -19,6 +20,7 @@ def post_detail(request, pk):
 
 # Публикуем новый пост через форму
 
+@login_required
 def post_new(request):
     if request.method == "POST":
     # т.е. request.POST
@@ -60,7 +62,22 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
-# Функция просмотра списка постов черновиков
+# Функция просмотра списка  черновых постов
+
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+# Функция публикации черновых постов
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+# Функция удаления постов
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
