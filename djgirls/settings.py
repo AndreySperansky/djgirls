4725d9b1abcb26599843b9c3fb3a29edc4e5df27
Andrey.Speransky <andrey.speransky@gmail.com>
2020-10-26 22:13:14 +0300

    Создали расширение шаюлона

--- djgirls/settings.py
+++ djgirls/settings.py
@@ -1,9 +1,11 @@
-
+from pathlib import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # BASE_DIR = 'D:\\_DJANGO\\dj_girls'
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 
@@ -110,5 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
