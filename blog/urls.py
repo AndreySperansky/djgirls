923bf205f5b34e1ba7ea93daba13213d815364f7
Andrey.Speransky <andrey.speransky@gmail.com>
2020-10-26 22:44:53 +0300

    Создали страницу post_detail.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
+'''
+<int:pk> — эта часть означает, что Django ожидает целочисленное значение и 
+преобразует его в представление — переменную pk
+'''
